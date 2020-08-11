287eee0429044fb6659e86821102b6b19222ca52
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-08-11 11:31:29 +0200

    Add Javadoc and fix a bug with getOptimalValidSubdivision (AKA getFirstValidSubdivision)

--- src/main/java/com/rayferric/dungeonfinder/DungeonConfiguration.java
+++ src/main/java/com/rayferric/dungeonfinder/DungeonConfiguration.java
@@ -1,5 +1,7 @@
 package com.rayferric.dungeonfinder;
 
+import com.rayferric.dungeonfinder.util.BlockPos;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +10,8 @@ import java.util.Objects;
 public class DungeonConfiguration {
     public DungeonConfiguration(List<BlockPos> dungeons) {
         this.dungeons = new ArrayList<BlockPos>(dungeons);
-        Collections.sort(this.dungeons); // We must sort the array, so two differently ordered configurations will remain equal
+        // We must sort the array, so two differently ordered configurations will remain equal
+        Collections.sort(this.dungeons);
 
         center = new BlockPos(0, 0, 0);
         int i = 0;
@@ -37,6 +40,11 @@ public class DungeonConfiguration {
         return String.format("DungeonConfiguration{dungeons=%s, center=%s}", dungeons, center);
     }
 
+    /**
+     * Check if all the spawners are at most 16 block away from the center
+     *
+     * @return    whether is the configuration valid
+     */
     public boolean isValid() {
         for(BlockPos pos : dungeons)
             if(center.distance(pos) > 16.0)
@@ -44,6 +52,11 @@ public class DungeonConfiguration {
         return true;
     }
 
+    /**
+     * Subdivides this configuration into two children with one less spawner each
+     *
+     * @return    list of two children configurations
+     */
     public List<DungeonConfiguration> subdivide() {
         List<DungeonConfiguration> subConfigs = new ArrayList<>(2);
 
@@ -58,21 +71,44 @@ public class DungeonConfiguration {
         return subConfigs;
     }
 
-    public DungeonConfiguration getFirstValidSubdivision(int minDungeons) {
+    /**
+     * Subdivides the configuration till a valid one that has at least minDungeons spawners is found<br>
+     * Always returns the configuration with the most spawners
+     *
+     * @param minDungeons    minimum number of dungeons in a configuration
+     *
+     * @return    a valid configuration with the most spawners possible
+     */
+    public DungeonConfiguration getOptimalValidSubdivision(int minDungeons) {
         if(isValid())return this;
         if(dungeons.size() <= minDungeons)return null;
         List<DungeonConfiguration> subConfigs = subdivide();
+        DungeonConfiguration candidate = null;
         for(DungeonConfiguration subConfig : subConfigs) {
-            DungeonConfiguration config = subConfig.getFirstValidSubdivision(minDungeons);
-            if(config != null)return config;
+            DungeonConfiguration config = subConfig.getOptimalValidSubdivision(minDungeons);
+            if(config != null)
+                if(candidate == null)
+                    candidate = config;
+                else if(config.dungeons.size() > candidate.dungeons.size())
+                    candidate = config;
         }
         return null;
     }
 
+    /**
+     * Returns a list of spawner positions in the configuration
+     *
+     * @return    list of spawner positions
+     */
     public List<BlockPos> getDungeons() {
         return dungeons;
     }
 
+    /**
+     * Returns the center point of the configuration which is an optimal (if not required) place for the player to stand
+     *
+     * @return    center of the configuration
+     */
     public BlockPos getCenter() {
         return center;
     }

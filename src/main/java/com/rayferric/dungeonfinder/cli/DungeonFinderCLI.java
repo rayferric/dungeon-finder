7925708f095bdef7130dd627e5f7af8874d21597
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-09-06 13:11:44 +0200

    Add --max-dist

--- src/main/java/com/rayferric/dungeonfinder/cli/DungeonFinderCLI.java
+++ src/main/java/com/rayferric/dungeonfinder/cli/DungeonFinderCLI.java
@@ -5,11 +5,8 @@ import com.rayferric.dungeonfinder.DungeonFinder;
 import com.rayferric.dungeonfinder.util.BlockPos;
 import org.apache.commons.cli.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
 public class DungeonFinderCLI {
     public static void main(String[] args) {
@@ -22,9 +19,14 @@ public class DungeonFinderCLI {
         Option maxXOption = new Option(null, "max-x", true, "most positive region's X position");
         Option minZOption = new Option(null, "min-z", true, "most negative region's Z position");
         Option maxZOption = new Option(null, "max-z", true, "most positive region's Z position");
-        Option minGroupSizeOption = new Option("c", "min-config-size", true, "minimum number of spawners per dungeon configuration (default: 3)");
-        Option numThreadsOption = new Option("t", "num-threads", true, "number of threads used to process regions (default: 8, keep in mind that processing speed heavily depends on drive performance, this exact value gives off best benchmark performance overall on an HDD, higher values may theoretically help on SSDs and faster drives)");
-        Option reportDelayOption = new Option("r", "report-delay", true, "delay between individual progress reports in milliseconds (default: 10000)");
+        Option minConfigSizeOption = new Option("c", "min-config-size", true,
+                "minimum number of spawners per dungeon configuration (default: 3)");
+        Option maxDistOption = new Option("d", "max-dist", true,
+                "maximum distance from center of configuration to a single spawner (default: 16)");
+        Option numThreadsOption = new Option("t", "num-threads", true,
+                "number of threads used to process regions (default: 8, keep in mind that processing speed heavily depends on drive performance, this exact value gives off best benchmark performance overall on an HDD, higher values may theoretically help on SSDs and faster drives)");
+        Option reportDelayOption = new Option("r", "report-delay", true,
+                "delay between individual progress reports in milliseconds (default: 10000)");
 
         worldDirectoryOption.setRequired(true);
         minXOption.setRequired(true);
@@ -39,7 +41,8 @@ public class DungeonFinderCLI {
         options.addOption(maxXOption);
         options.addOption(minZOption);
         options.addOption(maxZOption);
-        options.addOption(minGroupSizeOption);
+        options.addOption(minConfigSizeOption);
+        options.addOption(maxDistOption);
         options.addOption(numThreadsOption);
         options.addOption(reportDelayOption);
 
@@ -59,13 +62,17 @@ public class DungeonFinderCLI {
         int maxX = Integer.parseInt(cmd.getOptionValue("max-x"));
         int minZ = Integer.parseInt(cmd.getOptionValue("min-z"));
         int maxZ = Integer.parseInt(cmd.getOptionValue("max-z"));
-        int minConfigSize = cmd.hasOption("min-config-size") ? Integer.parseInt(cmd.getOptionValue("min-config-size")) : 3;
+        int minConfigSize =
+                cmd.hasOption("min-config-size") ? Integer.parseInt(cmd.getOptionValue("min-config-size")) : 3;
+        int maxDist = cmd.hasOption("max-dist") ? Integer.parseInt(cmd.getOptionValue("max-dist")) : 16;
         int numThreads = cmd.hasOption("num-threads") ? Integer.parseInt(cmd.getOptionValue("num-threads")) : 8;
         int reportDelay = cmd.hasOption("report-delay") ? Integer.parseInt(cmd.getOptionValue("report-delay")) : 10000;
 
         DungeonFinder dungeonFinder = new DungeonFinder();
-        dungeonFinder.onStart(() -> System.out.println(String.format("Processing %d regions on %d threads...", (maxX - minX + 1) * (maxZ - minZ + 1), numThreads)));
-        dungeonFinder.onFilter((numFound, timeElapsed) -> System.out.println(String.format("Found %d dungeons. (%d s)\nStarted proximity filtering...", numFound, timeElapsed / 1000)));
+        dungeonFinder.onStart(() -> System.out.printf(
+                "Processing %d regions on %d threads...\n", (maxX - minX + 1) * (maxZ - minZ + 1), numThreads));
+        dungeonFinder.onFilter((numFound, timeElapsed) -> System.out.printf(
+                "Found %d dungeons. (%d s)\nStarted proximity filtering...\n", numFound, timeElapsed / 1000));
         dungeonFinder.onReport((numComplete, numTotal, timeElapsed) -> {
             double timeRemaining = 0;
             if(numComplete != 0)
@@ -74,23 +81,26 @@ public class DungeonFinderCLI {
 
             double progress = (double)numComplete / numTotal * 100.0;
 
-            System.out.println(String.format("%d %% - ETA %s s", (long)progress, remainingTimeStr));
+            System.out.printf("%d %% - ETA %s s\n", (long)progress, remainingTimeStr);
         });
 
         List<DungeonConfiguration> dungeonConfigs = null;
         try {
-            dungeonConfigs = dungeonFinder.run(worldDirectory, minX, maxX, minZ, maxZ, minConfigSize, numThreads, reportDelay);
+            dungeonConfigs = dungeonFinder
+                    .run(worldDirectory, minX, maxX, minZ, maxZ, minConfigSize, maxDist, numThreads, reportDelay);
         } catch(IOException e) {
             System.err.println("Failed to open world directory.");
             e.printStackTrace();
             System.exit(1);
         }
 
-        System.out.println(String.format("Found %d dungeon configuration%s with size of at least %d:", dungeonConfigs.size(), dungeonConfigs.size() == 1 ? "" : "s", minConfigSize));
+        System.out.printf("Found %d dungeon configuration%s with size of at least %d:\n", dungeonConfigs.size(),
+                dungeonConfigs.size() == 1 ? "" : "s", minConfigSize);
         for(DungeonConfiguration config : dungeonConfigs) {
             BlockPos center = config.getCenter();
             int spawnerCount = config.getDungeons().size();
-            System.out.println(String.format("%d %d %d (%s spawner%s)", center.getX(), center.getY(), center.getZ(), spawnerCount, spawnerCount == 1 ? "" : "s"));
+            System.out.printf("%d %d %d (%s spawner%s)\n", center.getX(), center.getY(), center.getZ(), spawnerCount,
+                    spawnerCount == 1 ? "" : "s");
         }
     }
 
@@ -118,10 +128,10 @@ public class DungeonFinderCLI {
         }
 
         if(cmd.hasOption("version")) {
-            System.out.println(String.format("Dungeon Finder %s", VERSION));
+            System.out.printf("Dungeon Finder %s\n", VERSION);
             System.exit(0);
         }
     }
 
-    private final static String VERSION = "1.0.1";
+    private final static String VERSION = "1.1.0";
 }

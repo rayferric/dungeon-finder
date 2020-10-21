9aa0fb5adc7f57db0c4e9748aecbe1eb22f99c39
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-10-21 16:47:52 +0200

    Add mob types

---
+++ src/main/java/com/rayferric/dungeonfinder/Spawner.java
@@ -0,0 +1,64 @@
+package com.rayferric.dungeonfinder;
+
+import com.conversantmedia.util.collection.geometry.Point3d;
+import com.conversantmedia.util.collection.geometry.Rect3d;
+import com.conversantmedia.util.collection.spatial.HyperPoint;
+import com.conversantmedia.util.collection.spatial.HyperRect;
+import com.conversantmedia.util.collection.spatial.RectBuilder;
+import com.rayferric.dungeonfinder.util.MobType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class Spawner {
+    public static final class Builder implements RectBuilder<Spawner> {
+        @Override
+        public HyperRect<?> getBBox(@NotNull Spawner spawner) {
+            double x = spawner.pos.getCoord(0);
+            double y = spawner.pos.getCoord(1);
+            double z = spawner.pos.getCoord(2);
+            return new Rect3d(x, y, z, x, y, z);
+        }
+
+        @Override
+        public HyperRect<?> getMbr(@NotNull HyperPoint p1, @NotNull HyperPoint p2) {
+            double x1 = p1.getCoord(0);
+            double y1 = p1.getCoord(1);
+            double z1 = p1.getCoord(2);
+            double x2 = p2.getCoord(0);
+            double y2 = p2.getCoord(1);
+            double z2 = p2.getCoord(2);
+            return new Rect3d(x1, y1, z1, x2, y2, z2);
+        }
+    }
+
+    public Spawner(@NotNull Point3d pos, @NotNull MobType type) {
+        this.pos = pos;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Spawner spawner = (Spawner)o;
+        return Objects.equals(pos, spawner.pos) &&
+                type == spawner.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pos, type);
+    }
+
+    public Point3d getPos() {
+        return pos;
+    }
+
+    public MobType getType() {
+        return type;
+    }
+
+    private final Point3d pos;
+    private final MobType type;
+}
---
+++ src/main/java/com/rayferric/dungeonfinder/util/MobType.java
@@ -0,0 +1,58 @@
+package com.rayferric.dungeonfinder.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public enum MobType {
+    NONE("none"),
+    CREEPER("creeper"),
+    SKELETON("skeleton"),
+    SPIDER("spider"),
+    GIANT("giant"),
+    ZOMBIE("zombie"),
+    SLIME("slime"),
+    GHAST("ghast"),
+    ZOMBIE_PIGMAN("zombie_pigman"),
+    ENDERMAN("enderman"),
+    CAVE_SPIDER("cave_spider"),
+    SILVERFISH("silverfish"),
+    BLAZE("blaze"),
+    MAGMA_CUBE("magma_cube"),
+    ENDER_DRAGON("ender_dragon"),
+    WITHER("wither"),
+    BAT("bat"),
+    WITCH("witch"),
+    ENDERMITE("endermite"),
+    GUARDIAN("guardian"),
+    SHULKER("shulker"),
+    PIG("pig"),
+    SHEEP("sheep"),
+    COW("cow"),
+    CHICKEN("chicken"),
+    SQUID("squid"),
+    WOLF("wolf"),
+    MOOSHROOM("mooshroom"),
+    SNOW_GOLEM("snowman"),
+    OCELOT("ocelot"),
+    IRON_GOLEM("villager_golem"),
+    HORSE("horse");
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    public static MobType findById(@NotNull String id) {
+        for(MobType mob : values()) {
+            if(mob.id.equalsIgnoreCase(id))
+                return mob;
+        }
+        return NONE;
+    }
+
+    private final String id;
+
+    MobType(@NotNull String id) {
+        this.id = id;
+    }
+}
